525b332d1880e48f7ebe829db149ffc71239761d
mKomorowski <mkomorowski@windowslive.com>
2019-06-10 20:33:05 +0100

    feat(): Add createPublisher method

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import * as amqplib from 'amqplib';
 
+export type Publisher = (binding: string, message: string | object) => void;
+export enum exchangeTypes {
+  direct = 'direct',
+  fanout = 'fanout',
+  topic = 'topic',
+}
+
 class AMQP {
   private readonly url: string;
   private connection: amqplib.Connection | undefined;
@@ -23,9 +30,29 @@ class AMQP {
         connection
           .createChannel()
           .then((channel: amqplib.Channel) => resolve(channel))
-          .catch((err: Error) => reject(err));
+          .catch(reject);
       })
-      .catch((err: Error) => reject(err));
+      .catch(reject);
+    });
+  }
+
+  public createPublisher(exchange: string, type: exchangeTypes): Promise<Publisher> {
+    return new Promise((resolve, reject) => {
+      this.createChannel()
+        .then((channel: amqplib.Channel) => {
+          channel
+            .assertExchange(exchange, type)
+            .then(() => {
+              const publisher: Publisher = (key, message) => {
+                const messageString = typeof message === 'string' ? message : JSON.stringify(message);
+
+                channel.publish(exchange, key, Buffer.from(messageString));
+              };
+
+              resolve(publisher);
+            })
+            .catch(reject);
+      }).catch(reject);
     });
   }
 
@@ -39,9 +66,10 @@ class AMQP {
       .connect(this.url)
       .then((connection: amqplib.Connection) => {
         this.connection = connection;
+
         resolve(this.connection);
       })
-      .catch((err: Error) => reject(err));
+      .catch(reject);
     });
   }
 }
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,17 +1,20 @@
 import * as amqplib from 'amqplib';
 import { expect } from 'chai';
 import * as sinon from 'sinon';
-import AMQP from '../src/app';
+import AMQP, { exchangeTypes } from '../src/app';
 
 describe('AMQP', () => {
   let amqpConnectStub: any;
-  let amqpChannelStub: sinon.SinonStub;
+  let amqpChannelStub: any;
   let amqp: AMQP;
 
   beforeEach(() => {
     amqp = new AMQP('user', 'password', 'host', 'vhost', 5672);
     amqpConnectStub = sinon.stub(amqplib, 'connect');
-    amqpChannelStub = sinon.stub();
+    amqpChannelStub = {
+      assertExchange: sinon.stub().resolves(),
+      publish: sinon.spy(),
+    };
     amqpConnectStub.resolves({
       createChannel: sinon.stub().resolves(amqpChannelStub),
     });
@@ -59,6 +62,68 @@ describe('AMQP', () => {
       await amqp.createChannel();
 
       expect(amqpConnectStub.calledOnce).to.be.equal(true);
+    });
+  });
+
+  context('createPublisher', () => {
+    let exchange: string;
+    let type: exchangeTypes;
+
+    beforeEach(() => {
+      exchange = 'exchange';
+      type = exchangeTypes.topic;
+    });
+
+    it('should reject if amqplib createChannel throws an error', () => {
+      amqpConnectStub.resolves({
+        createChannel: sinon.stub().rejects(new Error('channel-error')),
+      });
+
+      amqp.createPublisher(exchange, type)
+        .then(() => expect.fail('amqplib createChannel should reject'))
+        .catch((err) => expect(err).to.be.instanceOf(Error));
+    });
+
+    it('should reject if channel assertExchange throws an error', () => {
+      amqpChannelStub.assertExchange.rejects(new Error('assert-error'));
+
+      amqp.createPublisher(exchange, type)
+        .then(() => expect.fail('amqplib assertExchange should reject'))
+        .catch((err) => expect(err).to.be.instanceOf(Error));
+    });
+
+    it('should call channel assertExchange with correct arguments', async () => {
+      await amqp.createPublisher(exchange, type);
+
+      expect(amqpChannelStub.assertExchange.withArgs(exchange, type).calledOnce).to.be.equal(true);
+    });
+
+    it('should resolve with a function', () => {
+      amqp.createPublisher(exchange, type)
+        .then((publish) => expect(publish).to.be.a('function'))
+        .catch(() => expect.fail('amqplib assertExchange should resolve'));
+    });
+
+    it('should call channel publish with correct arguments', async () => {
+      const binding = 'bind';
+      const message = 'message';
+
+      try {
+        const publish = await amqp.createPublisher(exchange, type);
+
+        publish(binding, message);
+        publish(binding, { message });
+
+        expect(amqpChannelStub.publish.withArgs(
+          exchange, binding, Buffer.from(message),
+        ).calledOnce).to.be.equal(true);
+
+        expect(amqpChannelStub.publish.withArgs(
+          exchange, binding, Buffer.from(JSON.stringify({ message })),
+        ).calledOnce).to.be.equal(true);
+      } catch ({}) {
+        expect.fail('amqplib assertExchange should resolve');
+      }
     });
   });
 });
